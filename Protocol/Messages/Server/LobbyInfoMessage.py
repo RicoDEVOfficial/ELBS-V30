fe23c518fdd21a2686b8583a4a90b5610f5b719f
RicoDEVOfficial <106838941+RicoDEVOfficial@users.noreply.github.com>
2025-04-22 04:36:17 +0200

    update lobbyinfo cause why not

--- Protocol/Messages/Server/LobbyInfoMessage.py
+++ Protocol/Messages/Server/LobbyInfoMessage.py
@@ -10,7 +10,7 @@ class LobbyInfoMessage(Writer):
 
     def encode(self):
         self.writeVInt(self.count)
-        self.writeString("Brawl Stars \n Version 30.242 \n Base made by BreadDEV \n Database Improvements by RicoDEV \n \n Database was rewritten in TinyDB, Bytestream & core adjusted to work with TinyDB \n \n Made with <3 by: \n \n Classic Brawl Team \n BreadDEV \n RicoDEV")
+        self.writeString("ELBS \n Version 30.242 \n Base made by BreadDEV \n Database Improvements by RicoDEV \n \n Database was rewritten in TinyDB, Bytestream & core adjusted to work with TinyDB \n \n Made with <3 by: \n \n Classic Brawl Team \n BreadDEV \n RicoDEV")
 
         self.writeVInt(0) # array
         for x in range(0):
